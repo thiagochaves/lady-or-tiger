25c0f7766deedc00c3c4b57d3720128994e4ee1d
Thiago Chaves <thiago.chaves@serpro.gov.br>
2022-07-11 17:25:32 -0300

    Purge all java and old files
    We are starting anew.
    For now we have the problems using the new syntax.

--- src/main/java/poc/minimize/Valor.java
+++
@@ -1,8 +0,0 @@
-package poc.minimize;
-
-enum Valor {
-    ZERO,
-    UM,
-    AMBOS,
-    QUALQUER
-}
--- src/main/java/poc/tableaux/ControladorParalelo.java
+++
@@ -1,9 +0,0 @@
-package poc.tableaux;
-
-interface ControladorParalelo {
-    void ramoNovo(Ramo novo);
-
-    void ramoFechado(int id);
-
-    void ramoAberto();
-}
